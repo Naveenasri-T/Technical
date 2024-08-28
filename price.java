b6ad3fc9796ace1afe0cb5886f4623597f2c3644
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2024-08-28 09:55:10 +0530

    Add files via upload

---
+++ price.java
@@ -0,0 +1,15 @@
+public class price {
+    String name ="";
+     int price=0;
+    public static void main(String[] args) {
+        price h=new price();
+        h.name="HP";
+        h.price=45;
+        price h1=new price();
+        h1.name="dell";
+        h1.price=344;
+        System.out.println(h.price);
+    }
+
+    
+}
