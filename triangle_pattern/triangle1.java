1a906a8ccdbae2b01c535be7fcbfb2c2645bcc4a
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2024-10-13 17:42:16 +0530

    Add files via upload

---
+++ triangle_pattern/triangle1.java
@@ -0,0 +1,32 @@
+package triangle_pattern;
+import java.util.Scanner;
+public class triangle1 {
+    
+    public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+        int i,j;
+        
+        System.out.println("enter te number");
+        int n= scan.nextInt();
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                
+                System.out.print( "*"+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
+
+    
+
+
+    
+
+
+
+
+    
+
