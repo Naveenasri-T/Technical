3b8761e9e42c0b69dac89b4a46d97b8365950fb6
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2024-10-23 18:43:37 +0530

    Add files via upload

---
+++ arrays/evenarray_elements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class evenarray_elements {
+    
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+            int N=scanner.nextInt();
+            int []arr=new int[N];
+            for(int i=0;i<N;i++){
+                arr[i]=scanner.nextInt();
+            }
+            for( int i=0;i<arr.length;i++){
+                if(arr[i]%2==0)
+                System.out.print(arr[i] +" ");
+            }
+
+        }
+    }
+    
