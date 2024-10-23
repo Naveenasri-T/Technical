b58edb7e798d3a6395137c7f372f8108b6de5f5e
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2024-10-23 18:42:20 +0530

    Add files via upload

---
+++ arrays/createarray.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class createarray{
+    public static void main(int String []args){
+        Scanner scanner=new Scanner(System.in){
+        int N=scanner.nextInt();
+        int 
+
+
+    }
+}
+}
